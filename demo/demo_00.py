ece06bc14d54a13790821e88c66ed129d169f0a2
Andrei Puchko <andrei.puchko@gmx.de>
2025-07-21 15:30:16 +0200

    demo

--- demo/demo_00.py
+++ demo/demo_00.py
@@ -39,12 +39,10 @@ def demo():
     report.load("test_data/report_02.json")
     report.params["p1"] = " <b>123</b> "
 
-    # report.run("temp/repo.html", data=demo_data)
-    # report.run("temp/repo.xlsx", data=demo_data)
-    result_file = BytesIO()
-    res_file = report.run(result_file, output_type="docx", data=demo_data)
-    print(res_file.getvalue())
-    # os.system(os.path.abspath(res_file))
+    # res_file =  report.run("temp/repo.html", data=demo_data)
+    res_file = report.run("temp/repo.xlsx", data=demo_data)
+    # res_file = report.run("temp/repo.docx", output_type="docx", data=demo_data)
+    os.system(os.path.abspath(res_file))
 
 if __name__ == "__main__":
     demo()
