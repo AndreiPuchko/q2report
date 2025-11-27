2330999b7fcae3ddf560ac9318e71080a4a90be5
Andrei Puchko <andrei.puchko@gmx.de>
2025-11-27 23:21:34 +0100

    demos

---
+++ demo/demo_06.py
@@ -0,0 +1,15 @@
+from q2report import Q2Report
+import json
+import os
+
+
+def demo():
+    report = Q2Report()
+    report.load(open("test_data/test-report-03.json").read())
+    data = json.load(open("test_data/test-data-03.json"))
+    res_file = report.run("temp/result.docx", data=data)
+    os.system(os.path.abspath(res_file))
+
+
+if __name__ == "__main__":  # pragma: no cover
+    demo()
